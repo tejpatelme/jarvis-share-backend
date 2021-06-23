20fbbf5148c1403ef139aeb9bbce111b6ec90296
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-23 05:08:25 +0000

    create : route to get single post

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -1,11 +1,13 @@
 require("express-async-errors");
 const { Router } = require("express");
 const { verifyToken } = require("../middlewares");
-const { createNewPost, getUsersPosts } = require("../controllers/posts.controller");
+const { createNewPost, getUsersPosts, getSinglePostById } = require("../controllers/posts.controller");
 
 const router = Router();
 
 router.route("/").get(verifyToken, getUsersPosts);
+
+router.route("/:postId").get(verifyToken, getSinglePostById);
 
 router.route("/newpost").post(verifyToken, createNewPost);
 
